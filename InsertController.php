fd9f0ec58fa09a4dc6e1dff46f56f47101904754
A-SH2k17 <alielsharkawy2007@gmail.com>
2023-12-22 19:51:24 +0200

    ll

--- InsertController.php
+++ InsertController.php
@@ -17,10 +17,27 @@ insert into post(img_url,title,published_at,is_veg,caption,user_id)
 values('Users/test/images/$imgUrl','$title',now(),$veg,'$desc','$user')
 ";
 $ret = mysqli_query($conn,$sql);
+$sql = "select * from post order by published_at desc limit 1";
 if($ret){
-    echo"Successful";
+    echo"Successful<br>";
+    $ret = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_assoc($ret);
+    $num = $row['pid'];
+    foreach($ingredients as $i){
+        $sql = "
+        INSERT INTO postingredient
+        VALUES ('$i',$num)
+        ";
+        $ret2 = mysqli_query($conn,$sql);
+        if($ret2){
+            echo"added $i<br>";
+        }else{
+            echo"failed to add $i";
+        }
+    }
 }else{
-    echo"failed";
+    echo"failed main Insert";
 }
+mysqli_free_result($ret);
 $conn -> close();
 ?>
